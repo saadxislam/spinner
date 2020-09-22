18bf9231d2b06f9ae7033623fa9f77f17c95edef
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-09-22 22:13:28 +0000

    minor change to the spinner

--- spinner1.js
+++ spinner1.js
@@ -21,21 +21,17 @@ setTimeout(() => {
 }, 900);
 
 setTimeout(() => {
-  process.stdout.write('\r|   ');
+  process.stdout.write('\r/   ')
 }, 1100);
 
 setTimeout(() => {
-  process.stdout.write('\r/   ')
+  process.stdout.write('\r-   ')
 }, 1300);
 
 setTimeout(() => {
-  process.stdout.write('\r-   ')
+  process.stdout.write('\r\\   ')
 }, 1500);
 
 setTimeout(() => {
-  process.stdout.write('\r\\   ')
-}, 1700);
-
-setTimeout(() => {
   process.stdout.write('\r|   ');
-}, 1900);
+}, 1700);
